507234fa7729c9d14e8b94016a2abe82d0e9188e
Prerna Panda <143763929+Prerna-1328@users.noreply.github.com>
2026-03-09 08:16:46 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World From Web-Hook!");
+        System.out.println("Hello World From Web-Hook Trigger!");
     }
 }
